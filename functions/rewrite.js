a764545358a2ad295fdec8afb4656db49fa2ac96
Mark Brocato <mbrocato@edg.io>
2024-07-11 22:15:23 +0300

    Try doc_end

--- functions/rewrite.js
+++ functions/rewrite.js
@@ -6,28 +6,28 @@ export async function handleHttpRequest(request, context) {
     (response) => {
       let transformedResponse = HtmlTransformer.stream(
         [
-          {
-            selector: "nav",
-            element: async (el) => {
-              el.set_attribute("style", "background: red")
-            },
-          },
           // {
-          //   // Since this operates on the document, there is no need to specify a selector
-          //   doc_end: async (d) => {
-          //     // Use the append() method to append the timestamp to the end of the document
-          //     // Specify 'html' as the second arguemnt to indicate the content is HTML, not plain text
-          //     d.append(
-          //       `<script>console.log('[Edgio] Response transformed at ${new Date().toISOString()} by Edgio')</script>`,
-          //       "html"
-          //     )
+          //   selector: "nav",
+          //   element: async (el) => {
+          //     el.set_attribute("style", "background: red")
           //   },
           // },
+          {
+            // Since this operates on the document, there is no need to specify a selector
+            doc_end: async (d) => {
+              // Use the append() method to append the timestamp to the end of the document
+              // Specify 'html' as the second arguemnt to indicate the content is HTML, not plain text
+              d.append(
+                `<script>console.log('[Edgio] Response transformed at ${new Date().toISOString()} by Edgio')</script>`,
+                "html"
+              )
+            },
+          },
         ],
         response
       )
       // Make any changes to the response headers here.
-      transformedResponse.headers.set("x-html-transformer-ran", "v3")
+      transformedResponse.headers.set("x-html-transformer-ran", "v4")
 
       // no cache
       transformedResponse.headers.set(
